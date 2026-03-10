d8f74c9777735dbbc8b9f94e8aceed131894f4d8
Abhishek Arjun <abhishekarjun1313@gmail.com>
2026-03-10 21:03:50 +0530

    Initial commit

---
+++ array.java
@@ -0,0 +1,12 @@
+public class array {
+    public static void main(String[] array){
+        int[] marks = new int[3];
+        marks[0] = 100;
+        marks[1] = 90;
+        marks[2] = 80;
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+       
+    }
+    
+}
